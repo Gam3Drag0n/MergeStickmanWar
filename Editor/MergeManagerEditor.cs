b2888a43bad169ffd7960f48a31e09f55f35c6a4
Gam3Drag0n <123709258+Gam3Drag0n@users.noreply.github.com>
2023-06-03 22:32:34 +0300

    Add files via upload

---
+++ Editor/MergeManagerEditor.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[CustomEditor(typeof(MergeManager))]
+public class MergeManagerEditor : Editor
+{
+    private MergeManager _mergeManager;
+
+
+    public void OnEnable()
+    {
+        _mergeManager = (MergeManager)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (_mergeManager.mergeMelee.Count > 0)
+        {
+            foreach (MergeMelee melee in _mergeManager.mergeMelee)
+            {
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.EndHorizontal();
+                if (GUILayout.Button("X", GUILayout.Width(20), GUILayout.Height(20)))
+                {
+                    _mergeManager.mergeMelee.Remove(melee);
+                    break;
+                }
+                melee.mergeID = EditorGUILayout.IntField("Уровень", melee.mergeID);
+                melee.damage = EditorGUILayout.FloatField("Урон", melee.damage);
+                melee.health = EditorGUILayout.IntField("Здоровье", melee.health);
+                melee.meleeType = (MeleeType)EditorGUILayout.EnumPopup("Тип милишника", melee.meleeType);
+                melee.characterPrefab = (GameObject)EditorGUILayout.ObjectField("Преваб тела", melee.characterPrefab, typeof(GameObject), false);
+
+                EditorGUILayout.EndVertical();
+            }
+        }
+        else EditorGUILayout.LabelField("Нет милишников");
+
+        if (GUILayout.Button("Добавить новый уровень милишника", GUILayout.Height(30))) _mergeManager.mergeMelee.Add(new MergeMelee());
+
+        if (_mergeManager.mergeRange.Count > 0)
+        {
+            foreach (MergeRange range in _mergeManager.mergeRange)
+            {
+                EditorGUILayout.BeginVertical();
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.EndHorizontal();
+                if (GUILayout.Button("X", GUILayout.Width(20), GUILayout.Height(20)))
+                {
+                    _mergeManager.mergeRange.Remove(range);
+                    break;
+                }
+                range.mergeID = EditorGUILayout.IntField("Уровень", range.mergeID);
+                range.damage = EditorGUILayout.FloatField("Урон", range.damage);
+                range.health = EditorGUILayout.IntField("Здоровье", range.health);
+                range.characterPrefab = (GameObject)EditorGUILayout.ObjectField("Преваб тела", range.characterPrefab, typeof(GameObject), false);
+                range.arrowPrefab = (GameObject)EditorGUILayout.ObjectField("Преваб стрелы", range.arrowPrefab, typeof(GameObject), false);
+    
+                EditorGUILayout.EndVertical();
+            }
+        }
+        else EditorGUILayout.LabelField("Нет дальников");
+
+        if (GUILayout.Button("Добавить новый уровень дальника", GUILayout.Height(30))) _mergeManager.mergeRange.Add(new MergeRange());
+
+        if (GUI.changed) SetObjectDirty(_mergeManager.gameObject);
+    }
+
+    public static void SetObjectDirty(GameObject obj)
+    {
+        EditorUtility.SetDirty(obj);
+        EditorSceneManager.MarkSceneDirty(obj.scene);
+    }
+}
---
+++ MergeManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MergeManager : MonoBehaviour
+{
+    public List<MergeMelee> mergeMelee;
+    public List<MergeRange> mergeRange;
+
+
+    public int GetMelee(int mergeID)
+    {
+        return mergeMelee.FindIndex(x => x.mergeID == mergeID);
+    }
+
+    public int GetRange(int mergeID)
+    {
+        return mergeRange.FindIndex(x => x.mergeID == mergeID);
+    }
+
+    public int GetMaxMergeID()
+    {
+        return mergeMelee.Count;
+    }
+}
+
+
+[System.Serializable]
+public class MergeMelee
+{
+    public int mergeID;
+    public float damage;
+    public int health;
+    public MeleeType meleeType;
+    public GameObject characterPrefab;
+}
+
+[System.Serializable]
+public class MergeRange
+{
+    public int mergeID;
+    public float damage;
+    public int health;
+    public GameObject characterPrefab;
+    public GameObject arrowPrefab;
+}
+
+[System.Serializable]
+public enum MeleeType
+{
+    oneHanded,
+    twoHanded,
+    twinBlades
+}
